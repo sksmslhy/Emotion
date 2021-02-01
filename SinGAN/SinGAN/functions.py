3b54f707498e6a6fcfb63c72a9cc7cac0dadf4f0
Hayun Lee <53000793+sksmslhy@users.noreply.github.com>
2021-02-01 13:44:04 +0900

    Update functions.py -- not cuda
    Specify map_location and annotated
    (# CPU Ver.)

--- SinGAN/SinGAN/functions.py
+++ SinGAN/SinGAN/functions.py
@@ -239,6 +239,10 @@ def load_trained_pyramid(opt, mode_='train'):
         Zs = torch.load('%s/Zs.pth' % dir)
         reals = torch.load('%s/reals.pth' % dir)
         NoiseAmp = torch.load('%s/NoiseAmp.pth' % dir)
+        # Gs = torch.load('%s/Gs.pth' % dir, map_location=torch.device('cpu'))
+        # Zs = torch.load('%s/Zs.pth' % dir, map_location=torch.device('cpu'))
+        # reals = torch.load('%s/reals.pth' % dir, map_location=torch.device('cpu'))
+        # NoiseAmp = torch.load('%s/NoiseAmp.pth' % dir, map_location=torch.device('cpu'))
     else:
         print('no appropriate trained model is exist, please train first')
     opt.mode = mode
